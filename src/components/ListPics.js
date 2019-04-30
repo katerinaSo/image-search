67fb107ec3173e817df74bda972be948902fceac
KaterinaSo <katya.solstad@gmail.com>
2019-04-29 22:13:46 -0400

    tiled imaged displayed

--- src/components/ListPics.js
+++ src/components/ListPics.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './ListPics.css';
+import ImageCard from './ImageCard'
 
 const ListPics=(props)=>{
-   const pics =  props.pics.map(({description,urls,id})=>{
-        return <img key={id} src={urls.small} alt={description}/>
+   const pics =  props.pics.map((pic)=>{
+        return <ImageCard key={pic.id} image={pic}/>
     });
-    return <div>{pics}</div>
+    return <div className="image-list">{pics}</div>
 
 }
 export default ListPics
